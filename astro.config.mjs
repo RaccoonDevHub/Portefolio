4fc2c6ae78e058e597b31729ee0f847bf78b20d4
RaccoonDevHub <mprovost.cim2@gmail.com>
2024-08-26 14:36:23 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://RaccoonDevHub.github.io',
-    base: '/Portefolio/src/pages/work.astro',
+    base: '/Portefolio',
   })
